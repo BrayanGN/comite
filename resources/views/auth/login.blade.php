1cae3fbe2dcd6335344824434a6cb367454ecc1a
19SERGIO <sergiolondono55@gmail.com>
2023-08-12 06:18:29 -0500

    vista

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+<div class="flex justify-center items-center h-screen mx-auto max-w-full">
+    <div class="bg-white p-8 shadow-md rounded-md w-80">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="text-xl font-semibold mb-4">{{ __('Bienvenido') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="block text-sm font-medium text-gray-700">{{ __('Correo Electronico') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="mt-1 px-3 py-2 block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -26,10 +26,10 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="block text-sm font-medium text-gray-700">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="mt-1 px-3 py-2 block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -71,3 +71,5 @@
     </div>
 </div>
 @endsection
+
+	
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,58 +7,62 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
+    @vite('resources/css/app.css')
 
-    {{-- <title>{{ config('app.name', 'Tutorial de Laravel 9 sobre Roles y Permisos de Usuario') }}</title> --}}
-
-    @vite(['resources/js/app.js'])
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  
 </head>
+    
 
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/login') }}">
-                    Logo
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
+<body class="bg-blue-100 mx-auto max-w-full">
+    <div id="app" >
+        @auth
+            
+        
+        <nav class="bg-blue-900 p-5">
+            <div class="container scroll-m-6 mx-auto">
+                <div class="flex items-center">
+                    <img src="{{ asset('img/logo-sena-negro-png-2022.png') }}" alt="Logo Sena " class="h-8 w-8">
+                    <span class="text-white ml-2 text-lg font-semibold">SE-JustApp</span>
+                </div>
+                <button class="flex items-center" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse align-text-left" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto"></ul>
+                    <ul class="flex justify-end"></ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
+                    <ul class="list-none">
+                        <!-- ... (rest of the code) -->
+                    </ul>
                         @guest
+                            
+                        
                             @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
+                                <li class="nav-item list-none flex justify-end">
+                                    <a class="text-white mr-4" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
                                 </li>
                             @endif
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
+                                <li class="nav-item list-none flex justify-end">
+                                    <a class="text-white mr-4" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
                                 </li>
                             @endif
                         @else
                             @role('Admin')
-                                <li><a class="nav-link" href="{{ route('users.index') }}">Usuarios</a></li>
+                                <li><a class="text-white mr-4" href="{{ route('users.index') }}">Usuarios</a></li>
                             @endrole
-                            <li><a class="nav-link" href="{{ route('roles.index') }}">Roles</a></li>
-                            <li><a class="nav-link" href="{{ route('products.index') }}">Productos</a></li>
-                            <li><a class="nav-link" href="{{ route('programas.index') }}">Programas</a></li>
-                            <li><a class="nav-link" href="{{ route('aprendizs.index') }}">Aprendices</a></li>
-                            <li><a class="nav-link" href="{{ route('comites.index') }}">Comites</a></li>
+                            <li><a class="text-white mr-4" href="{{ route('roles.index') }}">Roles</a></li>
+                            <li><a class="text-white mr-4" href="{{ route('products.index') }}">Productos</a></li>
+                            <li><a class="text-white mr-4" href="{{ route('programas.index') }}">Programas</a></li>
+                            <li><a class="text-white mr-4" href="{{ route('aprendizs.index') }}">Aprendices</a></li>
+                            <li><a class="text-white mr-4" href="{{ route('comites.index') }}">Comites</a></li>
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                <a id="navbarDropdown" class="text-white mr-4 dropdown-toggle" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
@@ -81,7 +85,8 @@
 
             </div>
         </nav>
-        <main class="py-4">
+        @endauth
+        <main class="flex justify-center items-center h-screen mx-auto max-w-full">
             <div class="container">
                 @yield('content')
             </div>
